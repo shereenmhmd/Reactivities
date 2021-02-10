bac31f6b9856968c739091323fa16e8ba3182596
Shereen <Shereen.mhmd@outlook.com>
2021-02-10 10:36:00 +0200

    intial commit section 2

---
+++ Persistence/DataContaxt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+
+        //inserting data into values table == Value intity
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Value>().HasData(
+                new Value { id = 1, name = "value 101" },
+                new Value { id = 2, name = "value 102" },
+                new Value { id = 3, name = "value 103" }
+            );
+        }
+    }
+}

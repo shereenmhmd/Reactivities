4b654861afa42b73970f87800985f90e79822e20
Shereen <Shereen.mhmd@outlook.com>
2021-03-01 14:18:27 +0200

    Section 13 login/register forms

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,13 +44,16 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email taken.");
+                ModelState.AddModelError("email", "Email taken.");
+                return ValidationProblem();
             }
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username taken.");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
             }
 
             var user = new AppUser
